b31009b0fb614c2613f39264292b324f6776ceb2
Alex-Trof <61555139+Alex-Trof@users.noreply.github.com>
2022-05-11 14:15:48 +0200

    Mise en place animation css

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # what-you-need
+
+Vous cherchez un article en particulier mais vous ne savez pas dans quel magasin il est disponible ? "What you need" cherchera pour vous, dites lui seulement ce que vous cherchez et dans quelle ville.
--- service.js
+++ service.js
@@ -1,11 +1,34 @@
 
-const source = document.getElementById('researchInput');
-const result = document.getElementById('result');
+window.onload = function() {
+  const article = document.getElementById('articleInput');
+  const city = document.getElementById('cityResearch');
+  const cityInput = document.getElementById('cityInput');
+  const button = document.getElementById('searchButton');
 
-const inputHandler = function(e) {
-  result.innerHTML = e.target.value;
+  const articleInputHandler = function(e) {
+    if(this.value.length > 3) {
+      city.style.opacity =  "1"
+      city.style.top = "100%"
+    }
+    else if (this.value.length <= 3) {
+      city.style.opacity =  "0"
+      city.style.top = "50%"
+      city.style.position = "absolute"
+    }
+  }
+
+  const cityInputHandler = function(e) {
+    if(this.value.length > 3) {
+      button.style.opacity =  "1"
+      button.style.top = "100%"
+    }
+    else {
+      button.style.opacity =  "0"
+      button.style.top = "70%"
+      button.style.position = "absolute"
+    }
+  }
+
+  article.addEventListener('input', articleInputHandler);
+  cityInput.addEventListener('input', cityInputHandler);
 }
-
-source.addEventListener('input', inputHandler);
-source.addEventListener('propertychange', inputHandler); // for IE8
-
